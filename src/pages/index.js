558682f4165bd327ad3cd56050bef5554183ca65
Ben Gubler <nebrelbug@gmail.com>
2019-08-30 20:39:18 -0600

    Fixed image source bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const features = [
     imageUrl: 'img/undraw/outer_space.svg',
     description: (
       <>
-        <a href={withBaseUrl('docs/v7/performance')}>Benchmarks</a> demonstrate
+        <a href='docs/v7/performance'>Benchmarks</a> demonstrate
         just how fast Squirrelly is. Think fast, then multiply by crazy speedy, and you have an idea of Squirrelly's performance.
       </>
     )
